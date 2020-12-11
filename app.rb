71b614bfab52683acfcf9cc93298983fe7245c1b
Zoe Kirsh <zoekirsh@gmail.com>
2020-12-10 17:30:28 -0800

    all passing

--- app.rb
+++ app.rb
@@ -2,5 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/team' do
+    erb :team
+  end
+
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team = params
+    erb :team
+  end
 
 end
